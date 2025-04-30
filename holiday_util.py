b424a6b171ecd6a64e75582b0a501fc8d0ba64e0
Fanwu <fanwu@star-net.cn>
2025-04-30 14:38:01 +0800

    rename for shadow naming

--- holiday_util.py
+++ holiday_util.py
@@ -3,20 +3,20 @@ import pandas as pd
 holiday_df = pd.read_csv("./data/chinese_holidays.csv")
 holiday_df["date"] = pd.to_datetime(holiday_df["date"])
 
-def merge_holiday(original_df: pd.DataFrame, holiday_df: pd.DataFrame) -> pd.DataFrame:
+def merge_holiday(original_df: pd.DataFrame, holiday_data: pd.DataFrame) -> pd.DataFrame:
     """
     Merge original dataframe with holiday dataframe to add Chinese holiday information.
 
     Args:
         original_df: Input dataframe containing date column 'pdate'
-        holiday_df: Holiday reference dataframe containing date and holiday information
+        holiday_data: Holiday reference dataframe containing date and holiday information
 
     Returns:
         pd.DataFrame: Merged dataframe with holiday information
     """
     result_df = pd.merge(
         original_df,
-        holiday_df[["date", "weekday", "description"]],
+        holiday_data[["date", "weekday", "description"]],
         left_on='pdate',
         right_on='date',
         how='left'
